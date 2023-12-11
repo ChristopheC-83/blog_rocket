d0b5285d58a5881b5156445f4f33dcfd4a80339f
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-12-11 15:12:55 +0100

    supression compte ok

--- public/javascript/profile_modifications.js
+++ public/javascript/profile_modifications.js
@@ -1,21 +1,40 @@
+// Elt de modification du mail
 const btnModifyMail = document.querySelector("#btnModifyMail");
 const formModifyMail = document.querySelector("#formModifyMail");
 
+// elt de modification du mot de passe
 const btnModifyPassword = document.querySelector("#btnModifyPassword");
 const formModifyPassword = document.querySelector("#formModifyPassword");
 
+// elt de suppression du compte
+const btnDeleteAccount=document.querySelector('#btnDeleteAccount')
+const cancelDelete=document.querySelector('#cancelDelete')
+const deleteAccountBlock=document.querySelector('#deleteAccountBlock')
 
 
 if (btnModifyMail) {
   btnModifyMail.addEventListener("click", function () {
     formModifyMail.classList.toggle("d-none");
     formModifyPassword.classList.add("d-none");
+    deleteAccountBlock.classList.add("d-none");
   });
 }
 if (btnModifyPassword) {
   btnModifyPassword.addEventListener("click", function () {
     formModifyPassword.classList.toggle("d-none");
     formModifyMail.classList.add("d-none");
+    deleteAccountBlock.classList.add("d-none");
   });
 }
-
+if (btnDeleteAccount) {
+  btnDeleteAccount.addEventListener("click", function () {
+    deleteAccountBlock.classList.toggle("d-none");
+    formModifyMail.classList.add("d-none");
+    formModifyPassword.classList.add("d-none");
+  });
+}
+if (cancelDelete) {
+  cancelDelete.addEventListener("click", function () {
+    deleteAccountBlock.classList.add("d-none");
+  });
+}
--- views/pages/user/profilePage.view.php
+++ views/pages/user/profilePage.view.php
@@ -18,17 +18,28 @@
                 <a href="" class="bg-primary text-light p-3 w-75 mx-auto text-center rounded-3 d-block text-decoration-none">Changer l'avatar pour une image personnelle.</a>
             </div>
             <div class="w-75 mx-auto my-5">
-                <p><span class="h3">Pseudo : </span> <?= $datasUser['login'] ?></p>
-                <p><span class="h3">Role : </span> <?= $datasUser['role'] ?></p>
-                <p id="btnModifyMail" class="cupo"><span class="h3">Mail : </span> <?= $datasUser['mail'] ?> <i class="fa-solid fa-pen"></i></p>
+                <p><span class="h3">Pseudo : <br> </span> <?= $datasUser['login'] ?></p>
+                <p><span class="h3">Role : <br></span> <?= $datasUser['role'] ?></p>
+                <p id="btnModifyMail" class="cupo"><span class="h3">Mail : <br></span> <?= $datasUser['mail'] ?> <i class="fa-solid fa-pen"></i></p>
+                <!-- bloc de modification de l'email  -->
                 <div class="d-none" id="formModifyMail">
                     <?php require_once("views/components/modifyMailBlock.php")  ?>
                 </div>
-                <p id="btnModifyPassword" class="cupo"><span class="h3">Mot de passe : </span> modification <i class="fa-solid fa-pen"></i></p>
-
+                <p id="btnModifyPassword" class="cupo"><span class="h3">Mot de passe : <br></span> modification <i class="fa-solid fa-pen"></i></p>
+                <!-- bloc de modification du mot de passe  -->
                 <div class="d-none" id="formModifyPassword">
                     <?php require_once("views/components/modifyPasswordBlock.php")  ?>
                 </div>
+                <div class="mt-3 w-100 mx-auto d-flex flex-column gap-3 justify-content-center align-items-center ">
+                    <button id="btnDeleteAccount" class="btn btn-danger w-100 fs-5 text-light fw-bold">Suppression du compte</button>
+                    <div class="w-100 d-none" id="deleteAccountBlock">
+                        <p class="text-danger text-center fw-bold fs-3">Je comprends que cette suppression <br>est définitive et irréversible</p>
+                        <button id="cancelDelete" class="btn btn-primary w-100 fs-5 text-light fw-bold mb-3">ANNULER</button>
+                        <a href="delete_account" class="w-100"><button class="btn btn-danger w-100 fs-5 text-light fw-bold">Je valide la supression de mon compte</button></a>
+
+                    </div>
+                </div>
+
             </div>
 
 
