7bea236ddacea16ef64bdd0fd92d30da60ebe433
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-12-17 20:21:45 +0100

    verification cas utilisateurs OK

--- views/pages/visitor/articlePage.view.php
+++ views/pages/visitor/articlePage.view.php
@@ -1,4 +1,4 @@
-<div class="container ">
+<div class="container row">
 
     <h3 class="text-center my-5 text-decoration-underline"><?= $article['pitch'] ?></h3>
 
@@ -15,31 +15,34 @@
         // on extrait les images du dossier
         $images = glob($directory . '/*');
         ?>
-        <div id="article_slider" class="carousel slide my-5 d-flex justify-content-center align-items-center">
-            <div class="carousel-indicators">
-                <?php for ($i = 0; $i < count($images); $i++) : ?>
-                    <button type="button" data-bs-target="#article_slider" data-bs-slide-to="<?= $i ?>" class="<?= $i === 0 ? "active" : "" ?>" aria-current="true" aria-label="Slide <?= $i ?>"></button>
-                <?php endfor ?>
-            </div>
-            <div class="carousel-inner row">
-                <?php for ($i = 0; $i < count($images); $i++) : ?>
-                    <div class="carousel-item m-0 col-12 col-md-10 col-lg-8 col-xl-6  <?= $i === 0 ? "active" : "" ?>">
-                        <img src="<?= URL . $images[$i] ?>" class="d-block w-100 " alt="...">
+        <div class=" col-md-10 col-lg-8 col-xl-6 mx-auto">
+            <div id="article_slider" class="carousel slide my-5 d-flex justify-content-center align-items-center w-100 w-lg-75 w-xl-50 mx-auto"
+            style="max-width:700px">
+                <div class="carousel-indicators">
+                    <?php for ($i = 0; $i < count($images); $i++) : ?>
+                        <button type="button" data-bs-target="#article_slider" data-bs-slide-to="<?= $i ?>" class="<?= $i === 0 ? "active" : "" ?>" aria-current="true" aria-label="Slide <?= $i ?>"></button>
+                    <?php endfor ?>
+                </div>
+                <div class="carousel-inner ">
+                    <?php for ($i = 0; $i < count($images); $i++) : ?>
+                        <div class="carousel-item m-0 <?= $i === 0 ? "active" : "" ?>">
+                            <img src="<?= URL . $images[$i] ?>" class="d-block w-100 " alt="...">
+                        </div>
+                    <?php endfor ?>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#article_slider" data-bs-slide="prev">
+                    <div class="p-1 overflow-hidden rounded-circle bg-primary d-flex justify-content-center align-items-center">
+                        <span class="carousel-control-prev-icon " aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
                     </div>
-                <?php endfor ?>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#article_slider" data-bs-slide="next">
+                    <div class="p-1 overflow-hidden rounded-circle bg-primary d-flex justify-content-center align-items-center">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </div>
+                </button>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#article_slider" data-bs-slide="prev">
-                <div class="p-1 overflow-hidden rounded-circle bg-primary d-flex justify-content-center align-items-center">
-                    <span class="carousel-control-prev-icon " aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </div>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#article_slider" data-bs-slide="next">
-                <div class="p-1 overflow-hidden rounded-circle bg-primary d-flex justify-content-center align-items-center">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </div>
-            </button>
         </div>
 
         <!-- si le media est une video -->
